a492bfdd813d59b36848725d8be5801a7517fc88
pruthvi <pruthviyadav37@gmail.com>
2022-06-11 20:28:44 +0530

    Background Services

---
+++ app/src/main/java/com/softhub/sensors/MainActivity.java
@@ -0,0 +1,122 @@
+package com.softhub.sensors;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    SensorManager sensorManager;
+    Sensor sensor;
+    TextView x_axis,y_axis,z_axis,max_x_axis,max_y_axis,max_z_axis;
+
+    private float lastX, lastY, lastZ;
+    private float deltaXMax = 0;
+    private float deltaYMax = 0;
+    private float deltaZMax = 0;
+    private float deltaX = 0;
+    private float deltaY = 0;
+    private float deltaZ = 0;
+    private float vibrateThreshold = 0;
+    Vibrator v;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        x_axis=findViewById(R.id.x_axis);
+        y_axis=findViewById(R.id.y_axis);
+        z_axis=findViewById(R.id.z_axis);
+        max_x_axis=findViewById(R.id.max_x_axis);
+        max_y_axis=findViewById(R.id.max_y_axis);
+        max_z_axis=findViewById(R.id.max_z_axis);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null){
+
+            sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            sensorManager.registerListener(MainActivity.this,sensor,SensorManager.SENSOR_DELAY_NORMAL);
+            vibrateThreshold = sensor.getMaximumRange() / 2;
+        }else {
+        }
+        v=(Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(MainActivity.this,sensor,SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(MainActivity.this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        displayCleanValues();
+        displayCurrentValues();
+        displayMaxValues();
+        deltaX = Math.abs(lastX - event.values[0]);
+        deltaY = Math.abs(lastY - event.values[1]);
+        deltaZ = Math.abs(lastZ - event.values[2]);
+
+        if (deltaX < 2)
+            deltaX = 0;
+        if (deltaY < 2)
+            deltaY = 0;
+        if (deltaZ < 2)
+            deltaZ = 0;
+
+        lastX = event.values[0];
+        lastY = event.values[1];
+        lastZ = event.values[2];
+
+        vibrate();
+    }
+    public void displayCleanValues(){
+        x_axis.setText("0.0");
+        y_axis.setText("0.0");
+        z_axis.setText("0.0");
+    }
+
+    public void displayCurrentValues(){
+        x_axis.setText(Float.toString(deltaX));
+        y_axis.setText(Float.toString(deltaY));
+        z_axis.setText(Float.toString(deltaZ));
+    }
+    public void displayMaxValues(){
+        if (deltaX > deltaXMax) {
+            deltaXMax = deltaX;
+            max_x_axis.setText(Float.toString(deltaXMax));
+        }
+        if (deltaY > deltaYMax) {
+            deltaYMax = deltaY;
+            max_y_axis.setText(Float.toString(deltaYMax));
+        }
+        if (deltaZ > deltaZMax) {
+            deltaZMax = deltaZ;
+            max_z_axis.setText(Float.toString(deltaZMax));
+        }
+    }
+
+    public void vibrate() {
+        if ((deltaX > vibrateThreshold) || (deltaY > vibrateThreshold) || (deltaZ > vibrateThreshold)) {
+            v.vibrate(50);
+        }
+    }
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
